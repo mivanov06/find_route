c83bda6054f6a804e6977d6169e260a644dbf834
mivanov06 <mivanov06@yandex.ru>
2021-07-22 17:55:36 +1000

    Навигационное меню

--- travel/urls.py
+++ travel/urls.py
@@ -21,5 +21,5 @@ from travel.views import home
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('cities/',include(('cities.urls', 'cities'))),
-    path('home', home),
+    path('', home, name='home'),
 ]
