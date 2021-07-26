78b425968a003b4cef7eea254eee5508823afa01
mivanov06 <mivanov06@yandex.ru>
2021-07-27 00:21:50 +1000

    Детализация с помощью DetailView

--- cities/views.py
+++ cities/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render, get_object_or_404
+from django.views.generic import DetailView
 
 from cities.models import City
 
 __all__ = (
-    'home',
+    'home', 'CityDetailView'
 )
 
 
@@ -16,3 +17,8 @@ def home(request, pk=None):
     qs = City.objects.all()
     context = {'objects_list': qs}
     return render(request, 'cities/home.html', context)
+
+
+class CityDetailView(DetailView):
+    queryset = City.objects.all()
+    template_name = 'cities/detail.html'
