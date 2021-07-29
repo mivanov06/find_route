70f5088ebb4b4df9b164377b35609136b4e007c0
mivanov06 <mivanov06@yandex.ru>
2021-07-29 23:16:52 +1000

    Привязка формы к модели, класс ModelForm

--- cities/views.py
+++ cities/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from django.views.generic import DetailView
 
+from cities.forms import HtmlForm, CityForm
 from cities.models import City
 
 __all__ = (
@@ -10,14 +11,18 @@ __all__ = (
 
 def home(request, pk=None):
     if request.method == "POST":
-        print(request.POST['name'])
+        form = CityForm(request.POST)
+        if form.is_valid():
+            print(form.cleaned_data)
+            form.save()
     if pk:
         # city = City.objects.filter(id=pk).first()
         city = get_object_or_404(City, id=pk)
         context = {'object': city}
         return render(request, 'cities/detail.html', context)
+    form = CityForm()
     qs = City.objects.all()
-    context = {'objects_list': qs}
+    context = {'objects_list': qs, 'form': form}
     return render(request, 'cities/home.html', context)
 
 
