4595fe7e54aa01f9c5f1c0fecde4befc4743c98c
mivanov06 <mivanov06@yandex.ru>
2021-07-29 23:44:38 +1000

    Использование Bootstrap для умучшения отображения формы

--- cities/forms.py
+++ cities/forms.py
@@ -8,7 +8,12 @@ class HtmlForm(forms.Form):
 
 
 class CityForm(forms.ModelForm):
+    name = forms.CharField(label='Город',
+                           widget=forms.TextInput(attrs=
+                                                  {'class': 'form-control',
+                                                   'placeholder': 'Введите название города'}
+                                                  ))
 
     class Meta:
         model = City
-        fields = ('name', )
+        fields = ('name',)
