b1b6c6566d576ac7fc20557fdb1ace9aaaeedeee
mivanov06 <mivanov06@yandex.ru>
2021-07-31 00:49:22 +1000

    UpdateView. Редактирование

--- cities/urls.py
+++ cities/urls.py
@@ -8,5 +8,6 @@ urlpatterns = [
     path('', home, name='home'),
     path('detail/<int:pk>/', CityDetailView.as_view(), name='detail'),
     path('add/', CityCreateView.as_view(), name='create'),
+    path('update/<int:pk>/', CityUpdateView.as_view(), name='update'),
 
 ]
