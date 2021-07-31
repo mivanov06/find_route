eec2cd8e2cd5fd258ddcc58863e67121ad726028
mivanov06 <mivanov06@yandex.ru>
2021-07-31 16:32:32 +1000

    7.38 Переопределение метода get_context_data

--- cities/views.py
+++ cities/views.py
@@ -64,3 +64,10 @@ class CityListView(ListView):
     paginate_by = 2
     model = City
     template_name = 'cities/home.html'
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+            context = super().get_context_data(**kwargs)
+            form = CityForm()
+            context['form'] = form
+            return context
+
