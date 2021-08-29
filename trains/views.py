7349d6db8f7c582ba44f989888416c46866044e8
mivanov06 <mivanov06@yandex.ru>
2021-08-29 23:51:03 +1000

    9.48 Создание формы TrainForm

--- trains/views.py
+++ trains/views.py
@@ -5,7 +5,7 @@ from django.shortcuts import render, get_object_or_404
 from django.urls import reverse_lazy
 from django.views.generic import DetailView, CreateView, UpdateView, DeleteView, ListView
 
-# from trains.forms import HtmlForm, TrainForm
+from trains.forms import TrainForm
 from trains.models import Train
 
 __all__ = (
