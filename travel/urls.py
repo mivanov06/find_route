fa63c7752634a910fe1e60ecb42d6232204c4698
mivanov06 <mivanov06@yandex.ru>
2021-09-01 00:09:25 +1000

    10.51 Форма для поиска маршрута

--- travel/urls.py
+++ travel/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
-from travel.views import home
+from routes.views import home
 
 urlpatterns = [
     path('admin/', admin.site.urls),
