75f3fda0bbb2a7b741aab692ec78b94c99439bde
mivanov06 <mivanov06@yandex.ru>
2021-09-04 23:15:44 +1000

    10.53 Функция отображения для поиска маршрутов

--- routes/views.py
+++ routes/views.py
@@ -1,7 +1,18 @@
 from django.shortcuts import render
 from routes.forms import RouteForm
+from django.contrib import messages
 
 
 def home(request):
     form = RouteForm()
     return render(request, 'routes/home.html', {'form': form})
+
+
+def find_routes(request):
+    if request.method == "POST":
+        form = RouteForm(request.POST)
+        return render(request, 'routes/home.html', {'form': form})
+    else:
+        form = RouteForm()
+        messages.error(request, "Нет данных для поиска")
+        return render(request, 'routes/home.html', {'form': form})
