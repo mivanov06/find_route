4dc01e238f271662388795a9a5777624aafdca13
mivanov06 <mivanov06@yandex.ru>
2021-09-05 11:39:06 +1000

    10.54 Функция поиска маршрута get_route

--- routes/views.py
+++ routes/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 from routes.forms import RouteForm
 from django.contrib import messages
 
+from utils import get_routes
+
 
 def home(request):
     form = RouteForm()
@@ -11,6 +13,13 @@ def home(request):
 def find_routes(request):
     if request.method == "POST":
         form = RouteForm(request.POST)
+        if form.is_valid():
+            try:
+                context = get_routes(request, form)
+            except ValueError as e:
+                messages.error(request, e)
+                return render(request, 'routes/home.html', {'form': form})
+        return render(request, 'routes/home.html', context)
         return render(request, 'routes/home.html', {'form': form})
     else:
         form = RouteForm()
